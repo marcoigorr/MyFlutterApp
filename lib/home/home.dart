06c97224d6c0a697715b3a00b570b2a83cb0d6b8
marcoigorr <petronio.mrc@gmail.com>
2022-11-09 10:47:59 +0100

    creating home context

--- lib/home/home.dart
+++ lib/home/home.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_literals_to_create_immutables
+
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -6,13 +8,34 @@ class HomePage extends StatelessWidget {
    @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        title: const Text('Home'),
+      ),
       
       body: Column(
         children: [
-          
-        ]
+          const SizedBox(height: 50,),
+
+          const Center(
+            child: Text('HomePage'),
+          )
+        ],
       ),
+
+      bottomNavigationBar: BottomNavigationBar(items: [
+        const BottomNavigationBarItem(
+          label: 'Code',
+          icon: Icon(Icons.code),
+        ),
+        const BottomNavigationBarItem(
+          label: 'Home',
+          icon: Icon(Icons.home),
+        ),
+        const BottomNavigationBarItem(
+          label: 'About Me',
+          icon: Icon(Icons.person),
+        ),
+      ]),
     );
   }
 }
